5e43e090954f6e6006946ea681301c9094ad58c6
satyamsharma_tatadigital <satyam.sharma@tatadigital.com>
2023-02-14 00:16:02 +0530

    gc01: commit-1 project added

---
+++ mod_1_graded_ass_1/lib/gc01.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class GC01 extends StatelessWidget {
+  const GC01({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Web", // This is the title of the Flutter web app
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Module 1 GC 01"),
+          ),
+        ),
+        body: const Center(
+          child: Text("Hello Module 1 GC 01"),
+        ),
+      ),
+    );
+  }
+}
---
+++ mod_1_graded_ass_1/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'GC01.dart';
+
+void main() {
+  runApp(const GC01());
+}
